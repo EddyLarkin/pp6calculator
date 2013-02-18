998991fc46a6d314fbdd0f2bc0fcce766dd37153
Eddy Larkin <E.J.P.Larkin@warwick.ac.uk>
2013-02-18 15:42:16 +0000

    Place operations inside seperate functions.

--- cppex/ex3.cpp
+++ cppex/ex3.cpp
@@ -1,52 +1,103 @@
 #include <iostream>
 #include <string>
+#include <climits>
+
+double multiply(double, double);
+double divide(double, double);
+double add(double, double);
+double subtract(double, double);
 
 int main(){
+	// allows user to specify and evaluate an operation (addition, multiplication...)
 	char opt;
 	std::string type;
 	double a, b, c;
 	int except = 0;
+	/*   Error types:
+ 	 *	0 = program succeeded
+	 *  	1 = invalid option
+	 *  	2 = divide by zero error
+	 *  	3 = user abort
+ 	*/
 	
-	std::cout << "Enter an operation type; (M)ultiply, (D)ivide, (A)dd or (S)ubtract:" << std::endl;
-	std::cin >> opt;
-	std::cout << " - First number:" << std::endl;
-	std::cin >> a;
-	std::cout << " - Second number:" << std::endl;
-	std::cin >> b;
-	
-	switch (toupper(opt)){
-		case 'M':
-		c = a * b;
-		type = " times ";
-		break;
-		case 'D':
-		if (b == 0){
-			except = 2;
+	while (except != 3){	
+		// continue running until user abort is found
+		except = 0;
+		std::cout << "Enter an operation type; (M)ultiply, (D)ivide, (A)dd or (S)ubtract, or (Q)uit:" << std::endl;
+		std::cin >> opt;
+		std::cin.clear();
+		std::cin.ignore(INT_MAX, '\n');
+		
+		opt = toupper((char)opt);
+		if (opt == 'Q'){
+			except = 3;
+		}	
+		else if (opt == 'M' || opt == 'D' || opt == 'C' || opt == 'S'){
+			std::cout << " - First number:" << std::endl;
+			std::cin >> a;
+			std::cin.clear();
+			std::cin.ignore(INT_MAX, '\n');
+			std::cout << " - Second number:" << std::endl;
+			std::cin >> b;
+			std::cin.clear();
+			std::cin.ignore(INT_MAX, '\n');
+			switch (opt){
+				case 'M':
+				c = multiply(a, b);
+				type = " times ";
+				break;
+				case 'D':
+				if (b == 0){
+					except = 2;
+				}
+				else{
+					c = divide(a, b);
+				};
+				type = " over ";
+				break;
+				case 'A':
+				c = add(a, b);
+				type = " plus ";
+				break;
+				case 'S':
+				c = subtract(a, b);
+				type = " subtract ";
+				break;
+			};	
 		}
-		else{
-			c = a / b;
+		else {
+			except = 1;
 		};
-		type = " over ";
-		break;
-		case 'A':
-		c = a + b;
-		type = " plus ";
-		break;
-		case 'S':
-		c = a - b;
-		type = " subtract ";
-		break;
-		default:
-		except = 1;
-	};
 
-	if (except == 1){
-		std::cout << "Invalid operation specified!" << std::endl;
-	}
-	else if (except == 2){
-		std::cout << "Divide by zero error!" << std::endl;
-	}
-	else{
-		std::cout << a << type << b << " is " << c << std::endl;
+		switch (except){
+			case 1:
+			std::cout << "Invalid operation specified!" << std::endl;
+			break;
+			case 2:
+			std::cout << "Divide by zero error!" << std::endl;
+			break;
+			case 3:
+			std::cout << "Exiting..." << std::endl;
+			break;
+			default:
+			std::cout << a << type << b << " is " << c << std::endl;
+		};
 	};
 };	
+
+double multiply(double a, double b){
+	// returns the product of a and b
+	return a * b;
+};
+double divide(double a, double b){
+	// returns the product of a and b
+	return a / b;
+};
+double add(double a, double b){
+	// returns the product of a and b
+	return a + b;
+};
+double subtract(double a, double b){
+	// returns the product of a and b
+	return a - b;
+};
